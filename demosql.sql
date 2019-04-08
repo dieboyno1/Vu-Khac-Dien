ea2899aff5101cb13ce3dab91cf543850c4ced52
dieboyno1 <49368612+dieboyno1@users.noreply.github.com>
2019-04-08 15:36:40 +0700

    Update demosql.sql

--- demosql.sql
+++ demosql.sql
@@ -1,75 +1,67 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 06, 2017 at 03:13 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 08, 2019 lúc 10:35 AM
+-- Phiên bản máy phục vụ: 10.1.38-MariaDB
+-- Phiên bản PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `demosql`
+-- Cơ sở dữ liệu: `demosql`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `khachhang`
+-- Cấu trúc bảng cho bảng `khachhang`
 --
 
-CREATE TABLE IF NOT EXISTS `khachhang` (
-`id` int(50) NOT NULL,
+CREATE TABLE `khachhang` (
+  `id` int(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `masv` varchar(50) NOT NULL,
   `link` varchar(100) NOT NULL,
   `DTB` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `khachhang`
+-- Đang đổ dữ liệu cho bảng `khachhang`
 --
 
 INSERT INTO `khachhang` (`id`, `name`, `masv`, `link`, `DTB`) VALUES
-(7, 'Ngoc Trinh', 'PD01834', 'http://192.168.1.14/Android_Sever_MOB401/Anhthe/anhthe4.jpg', 7),
-(10, 'anh ong vang', 'pd0283', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe1.jpg', 9.7),
-(11, 'Khanh Nam', 'PD01834', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe1.jpg', 8.5),
-(12, 'Bui Vinh', 'PD018361', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe2.jpg', 8.2),
-(13, 'Minh Nhat', 'PD01242', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe3.jpg', 7),
-(14, 'Ngoc Hoi', 'PD019376', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe1.jpg', 8.3),
-(15, 'Thi No', 'PD01382', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe4.jpg', 8.8),
-(16, 'A Han', 'PD013834', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe5.jpg', 9.2),
-(17, 'Anh la Vo dich', 'PD91424', 'http://10.18.101.40/Android_Sever_MOB401/Anhthe/anhthe1.jpg', 9.8),
-(19, 'Huynh', 'Pd92742', 'null', 8),
-(21, 'ff', 'f', 'f', 0),
-(22, 'Them moi ten', 'pgo2u44', 'null', 9.5);
+(24, 'Vu Khac Dien', 'DTC155D4802010018', 'https://www.facebook.com/photo.php?fbid=2296376584018062&set=pcb.2296377054018015&type=3&theater', 8.4),
+(29, 'Ma The Thiem', 'DTC155D43556567', 'https://scontent.fhan4-1.fna.fbcdn.net/v/t1.0-9/56398278_2067417396712857_8599590803762839552_n.jpg?', 10),
+(30, 'Hoang Van Phu', 'DTC155D43556567454', 'https://scontent.fhan4-1.fna.fbcdn.net/v/t1.0-9/56547435_2065824750205455_7402165191632224256_n.jpg?', 7);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lichhoc`
+-- Cấu trúc bảng cho bảng `lichhoc`
 --
 
-CREATE TABLE IF NOT EXISTS `lichhoc` (
-`thu` int(11) NOT NULL,
+CREATE TABLE `lichhoc` (
+  `thu` int(11) NOT NULL,
   `monhoc` varchar(100) NOT NULL,
   `giaovien` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `lichhoc`
+-- Đang đổ dữ liệu cho bảng `lichhoc`
 --
 
 INSERT INTO `lichhoc` (`thu`, `monhoc`, `giaovien`) VALUES
-(2, 'Androi co ban t', 'namnv'),
 (3, 'android Nang Cao', 'namnv'),
 (4, 'Tin hoc co so', 'namnv'),
 (5, 'Unity 2D', 'namnv'),
@@ -79,19 +71,19 @@ INSERT INTO `lichhoc` (`thu`, `monhoc`, `giaovien`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lop11305`
+-- Cấu trúc bảng cho bảng `lop11305`
 --
 
-CREATE TABLE IF NOT EXISTS `lop11305` (
-`id` int(50) NOT NULL,
+CREATE TABLE `lop11305` (
+  `id` int(50) NOT NULL,
   `name` varchar(50) NOT NULL,
   `masv` varchar(50) NOT NULL,
   `link` varchar(100) NOT NULL,
   `DTB` double NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `lop11305`
+-- Đang đổ dữ liệu cho bảng `lop11305`
 --
 
 INSERT INTO `lop11305` (`id`, `name`, `masv`, `link`, `DTB`) VALUES
@@ -102,18 +94,18 @@ INSERT INTO `lop11305` (`id`, `name`, `masv`, `link`, `DTB`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `monhoc`
+-- Cấu trúc bảng cho bảng `monhoc`
 --
 
-CREATE TABLE IF NOT EXISTS `monhoc` (
-`id` int(50) NOT NULL,
+CREATE TABLE `monhoc` (
+  `id` int(50) NOT NULL,
   `tenmon` varchar(50) NOT NULL,
   `thuocnganh` varchar(50) NOT NULL,
   `linkpdf` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `monhoc`
+-- Đang đổ dữ liệu cho bảng `monhoc`
 --
 
 INSERT INTO `monhoc` (`id`, `tenmon`, `thuocnganh`, `linkpdf`) VALUES
@@ -124,58 +116,94 @@ INSERT INTO `monhoc` (`id`, `tenmon`, `thuocnganh`, `linkpdf`) VALUES
 (7, 'Unity 2D ', 'Mobile', 'https://drive.google.com/file/d/0BwJk9UVfV1Q2RjVDZTJlWWpTUDA/view?usp=sharing'),
 (8, 'Sever Android (php&nodejs)', 'Mobile', 'https://drive.google.com/file/d/0BwJk9UVfV1Q2czZ1YnZxOUxHbUk/view?usp=sharing');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `passs` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `Username`, `passs`) VALUES
+(1, 'Tien', '123');
+
+--
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `khachhang`
+-- Chỉ mục cho bảng `khachhang`
 --
 ALTER TABLE `khachhang`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lichhoc`
+-- Chỉ mục cho bảng `lichhoc`
 --
 ALTER TABLE `lichhoc`
- ADD PRIMARY KEY (`thu`);
+  ADD PRIMARY KEY (`thu`);
 
 --
--- Indexes for table `lop11305`
+-- Chỉ mục cho bảng `lop11305`
 --
 ALTER TABLE `lop11305`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `monhoc`
+-- Chỉ mục cho bảng `monhoc`
 --
 ALTER TABLE `monhoc`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `khachhang`
+-- AUTO_INCREMENT cho bảng `khachhang`
 --
 ALTER TABLE `khachhang`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
 --
--- AUTO_INCREMENT for table `lichhoc`
+-- AUTO_INCREMENT cho bảng `lichhoc`
 --
 ALTER TABLE `lichhoc`
-MODIFY `thu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `thu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
--- AUTO_INCREMENT for table `lop11305`
+-- AUTO_INCREMENT cho bảng `lop11305`
 --
 ALTER TABLE `lop11305`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
--- AUTO_INCREMENT for table `monhoc`
+-- AUTO_INCREMENT cho bảng `monhoc`
 --
 ALTER TABLE `monhoc`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
